c2736a061bba09a27554e1432cf85f5dfffb4586
yasmin <yasminefawzy22@gmail.com>
2019-09-07 20:42:54 +0200

    React Weather-App

---
+++ src/components/Forecasting.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Forecasting extends Component {
+  render() {
+    return (
+      <div>
+
+          <input type='text' />
+          <button>Search</button>
+        
+      </div>
+    )
+  }
+}
+
+export default Forecasting
