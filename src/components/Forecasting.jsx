ce6da58007e8cac0fffaf181f1edb5ed5d2500b9
yasmin <yasminefawzy22@gmail.com>
2019-09-07 21:33:15 +0200

    API key

--- src/components/Forecasting.jsx
+++ src/components/Forecasting.jsx
@@ -1,7 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {apiKey} from './token.js'
 
 class Forecasting extends Component {
+    componentDidMount(){
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=&APPID=&units=metric')
+    }
   render() {
+
     return (
       <div>
 
