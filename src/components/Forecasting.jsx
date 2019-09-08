4cc2deb22481b3599da0a875c38e39c1f93a9af3
yasmin <yasminefawzy22@gmail.com>
2019-09-08 13:13:15 +0200

    fetch url

--- src/components/Forecasting.jsx
+++ src/components/Forecasting.jsx
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import {apiKey} from './token.js'
 
 class Forecasting extends Component {
+
+    state={
+        input:''
+    }
     componentDidMount(){
-        fetch('http://api.openweathermap.org/data/2.5/weather?q=&APPID=&units=metric')
+        const api= apiKey
+        const url= `http://api.openweathermap.org/data/2.5/weather?q=&APPID${api}=&units=metric'
+        fetch()
+        .then(res => console.log(res))
+        .then(data=> console.log(data))
+            this.setState({ input : data
+
+            })
+        
+        
     }
   render() {
 
