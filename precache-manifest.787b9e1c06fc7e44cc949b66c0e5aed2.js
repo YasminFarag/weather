de2d62a1a44b3b49a38b0a601de63851b615770d
yasmin <yasminefawzy22@gmail.com>
2019-09-23 12:54:09 +0200

    Updates

---
+++ precache-manifest.787b9e1c06fc7e44cc949b66c0e5aed2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7487853a12a0ae7c1094ddd82cefaa31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51a570c92a42228ec2c1",
+    "url": "./static/css/2.8209e206.chunk.css"
+  },
+  {
+    "revision": "68ba9ae3610385c3ced5",
+    "url": "./static/css/main.9e56bf4a.chunk.css"
+  },
+  {
+    "revision": "51a570c92a42228ec2c1",
+    "url": "./static/js/2.83b49cf8.chunk.js"
+  },
+  {
+    "revision": "68ba9ae3610385c3ced5",
+    "url": "./static/js/main.92af5b7b.chunk.js"
+  },
+  {
+    "revision": "541d2f764136435da6e9",
+    "url": "./static/js/runtime~main.ced0df89.js"
+  },
+  {
+    "revision": "11e11f8cb0021ef992ac134be7900db6",
+    "url": "./static/media/clouds.11e11f8c.jpeg"
+  },
+  {
+    "revision": "7fdb1437d711b17a67f94e029afb367e",
+    "url": "./static/media/cloudy.7fdb1437.jpeg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "dfd7f7e3c3d623e50cad734e4b915c8e",
+    "url": "./static/media/lightning.dfd7f7e3.jpeg"
+  },
+  {
+    "revision": "84cd48d7e8f11fda53a883fc187471ea",
+    "url": "./static/media/snowy.84cd48d7.jpg"
+  },
+  {
+    "revision": "6495081ab9bd2c08a3081db71952a064",
+    "url": "./static/media/sunny.6495081a.jpeg"
+  },
+  {
+    "revision": "21b5a530306f4445d1e167824f3d9d5f",
+    "url": "./static/media/water-blue-ocean.21b5a530.jpg"
+  }
+]);
