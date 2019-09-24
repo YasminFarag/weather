b8b228d498f2155e5ea5a674369d2d1e1dc663d8
yasmin <yasminefawzy22@gmail.com>
2019-09-24 15:11:06 +0200

    add herokuapp.com to fetch data

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     weather=(e)=>{
       e.preventDefault()
     const city = e.target.elements.city.value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
   
   .then(res => res.json())
   .then(data=>  this.setState({
